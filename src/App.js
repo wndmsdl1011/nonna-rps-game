e97a06e48fc5a74a7e14bb99df53e6c91ebf9919
wndmsdl1011 <jueun7547@naver.com>
2024-07-05 00:02:01 +0900

    zzzz

--- src/App.js
+++ src/App.js
@@ -18,17 +18,61 @@ const choice = {
 
 }
 function App() {
-  const [userSelect,setUserSelect] = useState(null)
-
+  const [userSelect,setUserSelect] = useState(null);
+  const [computerSelect,setComputerSelect]=useState(null);
+  const [result,setResult] = useState("");
   const play=(userChoice)=>{
-    setUserSelect(choice[userChoice]) 
+    setUserSelect(choice[userChoice]); 
+    let computerChoice = randomChoice();
+    setComputerSelect(computerChoice);
+    judgement(choice[userChoice],computerChoice)
+  };
 
+  const judgement = (user,computer) => {
+    console.log("user",user,"computer",computer);
+
+    // 가위바위보 로직을 스스로 정의
+    // user == computer -> tie
+    // user == rock, computer == scissors -> user win
+    // user == rock, computer == paper -> user lose
+    // user == paper, computer == rock -> user win
+    // user == paper, computer == scissors -> user lose
+    // user == scissors, computer == paper -> user win
+    // user == scissors, computer == rock -> user lose
+
+    if(user.name == computer.name) {
+      return "tie"
+    }else if(user.name=="Rock"){
+      if(computer == "Scissors"){
+        return "win"
+      }else{
+        return "lose"
+      }
+    }else if(user.name=="Paper"){
+      if(computer == "Rock"){
+        return "win"
+      }else{
+        return "lose"
+      }
+    }else if(user.name)
+
+    
   }
+
+  const randomChoice=()=>{
+    let itemArray = Object.keys(choice);
+    console.log("item array",itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    console.log("random value",randomItem);
+    let final = itemArray[randomItem];
+    return choice[final];
+  } 
+
   return (
     <div>
       <div className='main'>
-        <Box title="You" item={userSelect}/>
-        <Box title="Computer" />
+        <Box title="You" item={userSelect} result={result}/>
+        <Box title="Computer" item={computerSelect} result={result}/>
       </div>
 
       <div className='main'>
