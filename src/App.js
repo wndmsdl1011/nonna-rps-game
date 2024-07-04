5e8b6eebcd2b2ef6dace876c4550aba3040b5207
wndmsdl1011 <jueun7547@naver.com>
2024-07-05 01:39:02 +0900

    일단 제출

--- src/App.js
+++ src/App.js
@@ -4,32 +4,34 @@ import Box from './component/Box';
 
 const choice = {
   rock: {
-    name:"Rock",
-    img:"https://i.natgeofe.com/k/ab1c0ce9-21ea-4543-891a-ff496845b0cf/geology-igneous_3x2.jpg"
+    name: "Rock",
+    img: "https://i.natgeofe.com/k/ab1c0ce9-21ea-4543-891a-ff496845b0cf/geology-igneous_3x2.jpg"
   },
-  scissors:{
-    name:"Scissors",
-    img: 'https://cdn.imweb.me/thumbnail/20200514/7fc8b1411fa8d.png'
+  scissors: {
+    name: "Scissors",
+    img: 'https://i.namu.wiki/i/9uflE3hRwRIk5OZkdtWrMJuGrRzD1oci2v_UVG_QFIkNfdiK9eRsTAvmL6vkLgInO6-ix42vbqFPI5ctvI1qzXHaEDS_4ZnTLnIJnQF0ZT5oUuqWUoaoc_4gLAnqzf2ys7NCBkgPW2fblwikHp2S_Q.webp'
   },
   paper: {
-    name:"Paper",
+    name: "Paper",
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlBJrv6z6L3ixesiVIZcOoPfzCrQmS0IsE3A&s'
   }
 
 }
 function App() {
-  const [userSelect,setUserSelect] = useState(null);
-  const [computerSelect,setComputerSelect]=useState(null);
-  const [result,setResult] = useState("");
-  const play=(userChoice)=>{
-    setUserSelect(choice[userChoice]); 
+  const [userSelect, setUserSelect] = useState(null);
+  const [computerSelect, setComputerSelect] = useState(null);
+  const [result, setResult] = useState("");
+  const [result2, setResult2] = useState("");
+
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
-    judgement(choice[userChoice],computerChoice)
+    setResult(judgement(choice[userChoice], computerChoice));
+    setResult2(judgement2(computerChoice, choice[userChoice]))
   };
 
-  const judgement = (user,computer) => {
-    console.log("user",user,"computer",computer);
+  const judgement = (user, computer) => {
 
     // 가위바위보 로직을 스스로 정의
     // user == computer -> tie
@@ -40,39 +42,36 @@ function App() {
     // user == scissors, computer == paper -> user win
     // user == scissors, computer == rock -> user lose
 
-    if(user.name == computer.name) {
+    if (user.name === computer.name) {
       return "tie"
-    }else if(user.name=="Rock"){
-      if(computer == "Scissors"){
-        return "win"
-      }else{
-        return "lose"
-      }
-    }else if(user.name=="Paper"){
-      if(computer == "Rock"){
-        return "win"
-      }else{
-        return "lose"
-      }
-    }else if(user.name)
+    } else if (user.name === "Rock") return computer.name === "Scissors" ? "win" : "lose"
+    else if (user.name === "Scissors") return computer.name === "Paper" ? "win" : "lose"
+    else if (user.name === "Paper") return computer.name === "Rock" ? "win" : "lose"
+  };
 
-    
-  }
+  const judgement2 = (user, computer) => {
+    if (user.name === computer.name) {
+      return "tie"
+    } else if (user.name === "Rock") return computer.name === "Scissors" ? "win" : "lose"
+    else if (user.name === "Scissors") return computer.name === "Paper" ? "win" : "lose"
+    else if (user.name === "Paper") return computer.name === "Rock" ? "win" : "lose"
+  };
 
-  const randomChoice=()=>{
+
+  const randomChoice = () => {
     let itemArray = Object.keys(choice);
-    console.log("item array",itemArray);
+    console.log("item array", itemArray);
     let randomItem = Math.floor(Math.random() * itemArray.length);
-    console.log("random value",randomItem);
+    console.log("random value", randomItem);
     let final = itemArray[randomItem];
     return choice[final];
-  } 
+  }
 
   return (
     <div>
       <div className='main'>
-        <Box title="You" item={userSelect} result={result}/>
-        <Box title="Computer" item={computerSelect} result={result}/>
+        <Box title="You" item={userSelect} result={result} />
+        <Box title="Computer" item={computerSelect} result={result2} />
       </div>
 
       <div className='main'>
--- src/component/Box.js
+++ src/component/Box.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Box = (props) => {
   return (
-    <div className='box'>
+    <div className={`box ${props.result}`}>
         <h1>{props.title}</h1>
         <img className="item-img"src= {props.item && props.item.img}/>
         <h2>{props.result}</h2>    
